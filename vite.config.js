21bf207406c1ad90b6e64b052b9888b5936fcdab
heshan <heshandinushka98@gmail.com>
2024-05-31 19:54:57 +0530

    Changes in Deploy

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-
 export default defineConfig({
+  base: '/salary-calculator-2024-Q1-heshan98/', 
   build: {
     outDir: 'dist', 
   },
